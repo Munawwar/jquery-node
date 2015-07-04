5986d801a4387bd345a5c78a871a5451e2f6f0a0
Munawwar <munawwarfiroz@hotmail.com>
2015-07-04 14:11:33 +0400

    v0.0.1.

---
+++ jquery-node.js
@@ -0,0 +1,13 @@
+if (typeof define === 'function' && define.amd) {
+    define(['jquery'], function ($) {
+        return $;
+    });
+} else if (typeof exports === 'object') { //for unit tests
+    var jsdom = require('jsdom').jsdom,
+        window = jsdom('').parentWindow;
+
+    GLOBAL.document = window.document;
+    //(document.defaultView === window) is true
+
+    module.exports = require('jquery')(window);
+}
